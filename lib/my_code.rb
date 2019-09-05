cae8ea0b7f7a61b26f260dc0ecb3829b169aeaf7
Peter Werner <pcw1995@yahoo.com>
2019-09-05 20:08:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,11 +36,12 @@ end
 
 def reduce_to_total(source_array)
   i = 0 
+  total = 0 
   while source_array.length > i do 
-    tot_array = source_array[i] + source_array[i-1]
+    total += source_array[i] 
     i+=1 
   end 
-  tot_array
+  total
 end
 
 
