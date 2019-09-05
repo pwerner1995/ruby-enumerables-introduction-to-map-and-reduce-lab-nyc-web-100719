a04bd0ab20e2e90afdb840c89ba09f273e6f2a18
Peter Werner <pcw1995@yahoo.com>
2019-09-05 20:33:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -44,8 +44,15 @@ def reduce_to_total(source_array,starting_point = 0 )
   total
 end
 
-reduce_to_all_true(source_array)
-    
+def reduce_to_all_true(source_array)
+  i = 0 
+  value = true 
+  while source_array.length>i do 
+    if !source_array[i]
+      value = false 
+    end 
+    i =+1 
+  end 
 end 
 
 
