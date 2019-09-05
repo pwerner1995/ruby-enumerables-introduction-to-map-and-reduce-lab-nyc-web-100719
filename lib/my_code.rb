47874f1653e079c80266f7313843574143fdbbf7
Peter Werner <pcw1995@yahoo.com>
2019-09-05 20:13:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,19 +34,11 @@ def map_to_square(source_array)
   sqre_array
 end 
 
-def reduce_to_total(source_array)
-  i = 0 
-  total = 0 
-  while source_array.length > i do 
-    total += source_array[i] 
-    i+=1 
-  end 
-  total
-end
-
 def reduce_to_total(source_array,starting_point)
   i = 0 
-  total = starting_point 
+  if starting_point do 
+    total = starting_point
+  end  
   while source_array.length > i do 
     total += source_array[i] 
     i+=1 
