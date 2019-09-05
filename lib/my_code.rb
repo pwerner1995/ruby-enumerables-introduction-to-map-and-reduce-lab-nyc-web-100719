5b6e1ac5dc44a97ce0281cdfe3c99937eaa520c0
Peter Werner <pcw1995@yahoo.com>
2019-09-05 19:25:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,3 +23,13 @@ def map_to_double(source_array)
   end
   doub_array
 end
+
+def map_to_square(source_array)
+  sqre_array = []
+  i = 0 
+  while source_array.length >i do 
+    sqre_array[i] = source_array[i] **2 
+    i+=1 
+  end
+  sqre_array
+end 
